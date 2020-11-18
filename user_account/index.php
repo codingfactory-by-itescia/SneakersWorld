32a9af22c1c760f4408fae3945c856b208e15cbb
Axel <axel.demorest@gmail.com>
2020-11-18 10:15:47 +0100

    php

--- user_account/index.php
+++ user_account/index.php
@@ -3,11 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SneakersWorld</title>
 </head>
 <body>
     <?php
-        echo 'axel';
+        $servname = 'localhost';
+        $dbname = 'sneakersbdd';
+        $user = 'root';
+        $pass = 'root';
+        
+        try{
+            $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
+            $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $sql = "INSERT INTO Clients(Nom, Prenom)
+            VALUES ('Demorest', 'Axel')";
+        
+        $dbco->exec($sql);
+        echo 'donnée bien envoyée !';
+        }
+        
+        catch(PDOException $e){
+          echo "Erreur : " . $e->getMessage();
+        }
     ?>
 </body>
 </html>
