d4fd8dfddaf896504cc66724115ce78cc27b16df
Axel <axel.demorest@gmail.com>
2020-11-20 10:05:33 +0100

    CONNEXION

--- page-de-connexion/connect.php
+++ page-de-connexion/connect.php
@@ -25,9 +25,64 @@
             }
         </style>
 
+        <?php
+
+        require '../database/db.php';
+
+        if (!empty($_POST)) {
+
+            $errors = array();
+
+            if (empty($_POST['identifiant'])) {
+                $errors['identifiant'] = "Identifiant manquant.";
+            }
+
+            if (empty($_POST['password'])) {
+                $errors['password'] = "Mot de passe manquant.";
+            }
+
+            $req = $pdo->prepare('SELECT * FROM users WHERE mail = ?');
+
+            $req->execute([$_POST['identifiant']]);
+
+            $result = $req->fetch();
+            
+            if(empty($result)) {
+                $errors['error'] = "Utilisateur ou mot de passe incorrect.";
+            } else {
+
+                if(password_verify($_POST['password'], $result->password)) {
+
+                    $_SESSION['auth'] = $result;
+
+                    header('Location: ../user_account/profil.php');
+                }
+
+            }
+
+        }
+
+        ?>
+
         <header>
             <a class="button-menu" href="../page-accueil/accueil.php">Sneakers world</a>
         </header>
+
+        <?php if (!empty($errors)) : ?>
+            <div class="error-div">
+                <ul>
+
+                    <?php foreach ($errors as $error) : ?>
+
+                        <li><?= $error; ?></li>
+
+                    <?php endforeach; ?>
+
+                </ul>
+            </div>
+
+        <?php endif; ?>
+
         <div id="card">
             <div class="card-connect">
                 <h1>CONNEXION</h1>
@@ -35,7 +90,7 @@
                     <input name="identifiant" class="input-connect login" type="email" placeholder="Votre email" required>
                     <input name="password" class="input-connect password" type="password" placeholder="Mot de passe" required>
                     <div id="buttons">
-                        <a class="button" href="../page-inscription/signIn.html">Inscription</a>
+                        <a class="button" href="../page-inscription/signIn.php">Inscription</a>
                         <input class="button submitButton" type="submit" value="Connexion">
                     </div>
                 </form>
